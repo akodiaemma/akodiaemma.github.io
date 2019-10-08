a1027edc4f8d1c14b4f47886025d24c24c361028
Emmanuel Akodia <emmanuelakodia1@live.com>
2019-10-08 12:07:42 +0300

    Add files via upload

---
+++ js/simple.js
@@ -0,0 +1,123 @@
+
+tippy('#hotspot_1569356278238', {
+   content: "<h3>Eyepieces</h3><p>Operator views the specimen through the binocular eyepieces.  The binocular eyepieces provide a secondary magnification typically of 10x or 15x.  The spacing of the eyepieces is adjustable to suit the operator&rsquo;s face.</p><img class='compress' src='img/5.jpg'><img class='compress' src='img/4.jpg'>",
+   arrow: true,
+   trigger: 'click',
+   placement: 'right',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})   
+    
+tippy('#hotspo', {
+   content: '<h3>Objective lens turret</h3><p>Rotatable turret containing objective lenses of different magnifications.  The turret is rotated to bring the desired objective (hence magnification) to be used into the optical pathway.  The turret contains typically 5 different objective lenses having magnifications of 5, 10, 20, 50, and 100x.</p><img src="img/3.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'right',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})     
+        
+tippy('#hotspot_1569355330154', {
+   content: '<h3>Field diaphragm control</h3><p>Adjusts the size of the area of illumination of the specimen surface (typically is closed to just outside the field of view).</p><img src="img/1.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'top',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})    
+
+tippy('#hotspot_1569355658304', {
+   content: '<h3>Aperture diaphragm control</h3><p>Adjusts the numerical aperture of the microscope to optimise resolution, depth of field, and contrast of the image.</p><img src="img/1.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'left',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})    
+        
+tippy('#hotspot_1569356265041', {
+   content: '<h3>Focus control (coarse/fine)</h3><p>Used to adjust the focus of the image by raising/lowering the specimen stage relative to the objective lens.  Coarse control = outer knob;  fine control = inner knob.</p><img src="img/2.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'top',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})            
+
+
+//////RIGHT SIDE VIEW///////
+tippy('#hotspot_1569357255188', {
+   content: '<h3>Focus control (coarse/fine)</h3><p>Used to adjust the focus of the image by raising/lowering the specimen stage relative to the objective lens.  Coarse control = outer knob;  fine control = inner knob.</p><img src="img/2.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'right',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})  
+        
+tippy('#hotspot_1569357260099', {
+   content: '<h3>X-Y controller</h3><p>Used to move the stage in the x and y directions to view different areas of the specimen.</p>',
+   arrow: true,
+   trigger: 'click',
+   placement: 'right',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})  
+ 
+tippy('#hotspot_1569357265129', {
+   content: '<h3>Digital camera</h3><p>Captures digital images (micrographs) of the specimen surface.  The digital camera has its own eyepiece and functions separately to the operator’s binocular eyepieces.</p><img src="img/dc.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'right',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})  
+
+tippy('#hotspot_1569357400229', {
+   content: '<h3>Brightfield/darkfield filter selector rod</h3><p>Knob is pushed in to select normal brightfield illumination. When pulled out, a circular light stop is brought into the optical pathway to give darkfield illumination (oblique lighting).</p><img src="img/dd.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'right',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})  
+
+tippy('#hotspot_1569357277474', {
+   content: '<h3>Light source</h3><p>Lamphouse contains the light source which is typically a tungsten filament halogen lamp or a Xe/Zr/Na arc lamp.  Warning:  it is hot to the touch!</p><img src="img/8.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'right',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+}) 
+
+tippy('#hotspot_1569357633147', {
+   content: '<h3>Specimen stage</h3><p>Operator views the specimen through the binocular eyepieces.  The binocular eyepieces provide a secondary magnification typically of 10x or 15x.  The spacing of the eyepieces is adjustable to suit the operator’s face.</p><img src="img/10.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'right',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})  
+
+tippy('#hotspot_1569357785003', {
+   content: '<h3>Specimen</h3><p>The specimen to be examined typically is mounted in a polymer (epoxy or bakelite) mount (~25 mm diameter, ~10mm thick) and has been ground-polished-etched to reveal the microstructure of the material.</p><img src="img/a.jpg"><img src="img/11.jpg">',
+   arrow: true,
+   trigger: 'click',
+   placement: 'right',
+   animation: 'scale',
+   interactive: 'true',
+   theme: 'light',
+})          
+    
